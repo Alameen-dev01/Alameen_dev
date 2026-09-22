9d7eb5d2c5914524dd2b51ae0c71923a1bb47ac5
Alameen_dev <rumaalameen8@gmail.com>
2026-09-22 02:27:51 +0100

    Remove unused email verification import

--- Firebase.js
+++ Firebase.js
@@ -1,4 +1,3 @@
-import { sendEmailVerification } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
